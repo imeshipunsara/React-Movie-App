c52b50a1253cb887201aad3c2e73f382305c4599
Imeshi Punsara <imeshipunsara9@gmail.com>
2026-03-25 18:45:28 +0530

    update search movie function

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -29,10 +29,22 @@ function Home(){
     loadPopularMovies();
   }, []);
 
-    const handleSearch = (e) => {
+    const handleSearch = async(e) => {
         e.preventDefault();
-        alert(searchQuery);
-        setSearchQuery("");
+       if (!searchQuery.trim()) return
+       if (loading) return
+
+        setLoading(true);
+        try{
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        }catch(err){
+            console.log(err)
+            setError("faild to search movies...")
+        }finally{
+            setLoading(false)
+        }
     };
 
     return(
