16dfc2b4e2ddae210bab412b3f1a6f185aecdcf1
Imeshi Punsara <imeshipunsara9@gmail.com>
2026-03-25 15:17:05 +0530

    Create Favorite page

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,7 @@
+function Favorite(){
+    return <div className="favorites-empty">
+        <h2>No Favorte Movies Yet</h2>
+        <p>Start adding movies to your favorites and they will appear here</p>
+    </div>
+}
+export default Favorite
