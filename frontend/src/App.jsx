f7e08bf57b4cebf1b349ba741078629c56652ff5
Imeshi Punsara <imeshipunsara9@gmail.com>
2026-03-25 15:49:59 +0530

    create navbar component and add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
-
+import NavBar from './components/NavBar';
 
 
 function App() {
 
   return (
+    <div>
+      <NavBar/>
     <main className="main-content">
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/favorites' element={<Favorites/>}/>
         </Routes>
     </main>
+    </div>
   )
 }
 
